30d715366a924d3a315a78d81e8c4e9cdade1f7b
zrr <zrr@meail.com>
2018-11-22 12:17:58 +0000

    测试1

--- day01/Test01.java
+++ day01/Test01.java
@@ -3,6 +3,8 @@ package day01;
 public class Test01 {
 
 	public static void main(String[] args) {
+				System.out.println("test1");
+
 		System.out.println("善学如春起之苗");
 		System.out.println("不见其增，日渐其长");
 		System.out.println("假学如磨刀之石");
